d5568b5f6c60e34121b2ef87d03249fc419ac682
Keston C. Cole <163892335+KestonCCole5654@users.noreply.github.com>
2025-05-04 13:56:19 -0500

    Update page.tsx

--- client/src/pages/Onboarding/page.tsx
+++ client/src/pages/Onboarding/page.tsx
@@ -1,9 +1,5 @@
 import React from "react";
 
 export default function OnboardingPage() {
-  return (
-    <div style={{ color: 'red', fontSize: 32, textAlign: 'center', marginTop: '2rem' }}>
-      HELLO FROM ONBOARDING PAGE
-    </div>
-  );
+  return <div>Hello from Onboarding!</div>;
 }

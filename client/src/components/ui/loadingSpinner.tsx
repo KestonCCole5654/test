fb83cb95c828061eafd34655f6bbbd383f32ea24
Keston C. Cole <163892335+KestonCCole5654@users.noreply.github.com>
2025-05-20 20:57:45 -0400

    Updated Loader Component

--- client/src/components/ui/loadingSpinner.tsx
+++ client/src/components/ui/loadingSpinner.tsx
@@ -1,14 +1,12 @@
 export const LoadingSpinner = () => (
-  <div className="flex justify-center items-center h-full">
-    <div className="flex items-center justify-center gap-3">
-      <div className="relative h-8 w-8">
-        <div className="absolute inset-0 rounded bg-green-700/20 animate-ping"></div>
-        <div className="relative h-full w-full rounded bg-green-700 flex items-center justify-center animate-pulse">
-          <span className="text-white font-bold text-lg">SB</span>
-        </div>
+  <div className="flex flex-col justify-center items-center h-full gap-3">
+    <div className="relative h-8 w-8">
+      <div className="absolute inset-0 rounded bg-green-700/20 animate-ping"></div>
+      <div className="relative h-full w-full rounded bg-green-700 flex items-center justify-center animate-pulse">
+        <span className="text-white font-bold text-lg">SB</span>
       </div>
-      <span className="text-gray-500">Just a moment ...</span>
     </div>
+    <span className="text-gray-500">Just a moment ...</span>
   </div>
 );
 

c7840bcc78aec2a98a05c5be87d2ba05e7b052fd
Keston C. Cole <163892335+KestonCCole5654@users.noreply.github.com>
2025-05-26 10:32:57 -0500

    Update

--- client/src/components/Layout/SidebarLayout.tsx
+++ client/src/components/Layout/SidebarLayout.tsx
@@ -1,13 +1,18 @@
 import React, { useState } from "react"
 import { Outlet } from "react-router-dom"
-import Header from "../Header/header"
+import Sidebar from "../Sidebar/sidebar"
 import { cn } from "../../lib/utils"
 
 const SidebarLayout = () => {
+  const [collapsed, setCollapsed] = useState(false)
+
   return (
-    <div className="min-h-screen flex flex-col font-cal-sans bg-gray-50">
-      <Header />
-      <main className="flex-1">
+    <div className="min-h-screen flex font-cal-sans bg-gray-50">
+      <Sidebar collapsed={collapsed} setCollapsed={setCollapsed} />
+      <main className={cn(
+        "flex-1 transition-all duration-300",
+        collapsed ? "ml-[70px]" : "ml-[250px]"
+      )}>
         <Outlet />
       </main>
     </div>

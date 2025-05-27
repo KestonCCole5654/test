949938375398f548a07ef4284b843c4af00e37c7
Keston C. Cole <163892335+KestonCCole5654@users.noreply.github.com>
2025-05-27 16:31:51 -0500

    Update

--- client/src/components/ui/loadingSpinner.tsx
+++ client/src/components/ui/loadingSpinner.tsx
@@ -2,7 +2,7 @@ export const LoadingSpinner = () => (
   <div className="flex flex-col justify-center items-center h-full gap-3">
     <div className="relative h-8 w-8">
       <div className="absolute inset-0 rounded bg-green-700/20 animate-ping"></div>
-      <div className="relative h-full w-full rounded bg-green-700 flex items-center justify-center animate-pulse">
+      <div className="relative h-full w-full p-10 flex items-center justify-center animate-pulse">
         <img src="/sheetbills-logo.svg" alt="SheetBills Logo" className="h-8 w-auto" />
       </div>
     </div>

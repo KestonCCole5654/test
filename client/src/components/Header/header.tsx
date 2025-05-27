8da5d35e15e51578d80f106cf46c473182e14f1e
Keston C. Cole <163892335+KestonCCole5654@users.noreply.github.com>
2025-05-27 02:32:47 -0500

    update

--- client/src/components/Header/header.tsx
+++ client/src/components/Header/header.tsx
@@ -80,8 +80,8 @@ const Header = () => {
           {/* Logo */}
           <div className="flex items-center">
             <Link to="/" className="flex items-center space-x-2">
-              <div className="h-10 w-10 rounded flex items-center justify-center">
-                <img src="/sheetbills-logo.svg" alt="SheetBills Logo" className="h-8 w-auto" />
+              <div className="h-12 w-12 rounded flex items-center justify-center">
+                <img src="/sheetbills-logo.svg" alt="SheetBills Logo" className="h-12 w-auto" />
               </div>
               <span className="text-xl font-normal text-slate-200">SheetBills </span>
             </Link>

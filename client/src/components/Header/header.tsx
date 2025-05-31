01f9ecae0eb2868e6ee044531602c8472537b691
Keston C. Cole <163892335+KestonCCole5654@users.noreply.github.com>
2025-05-31 13:11:16 -0500

    Update

--- client/src/components/Header/header.tsx
+++ client/src/components/Header/header.tsx
@@ -98,13 +98,7 @@ const Header = () => {
                 >
                  Invoices
                 </Link>
-                <Link
-                  to="/customers"
-                  className="px-3 py-2 rounded-md font-normal text-slate-200 hover:text-slate-300  transition-colors"
-                  onClick={handleNavigation}
-                >
-                  Customers
-                </Link>
+                
                 <Link
                   to="/settings"
                   className="px-3 py-2 rounded-md font-normal text-slate-200 hover:text-slate-300  transition-colors"
@@ -206,13 +200,7 @@ const Header = () => {
                 >
                  Invoices
                 </Link>
-                <Link
-                  to="/customers"
-                  className="px-3 py-2.5 rounded-md text-slate-700 hover:text-slate-900 transition-colors"
-                  onClick={handleNavigation}
-                >
-                  Customers
-                </Link>
+                
                 <Link
                   to="/settings"
                   className="px-3 py-2.5 rounded-md text-slate-700 hover:text-slate-900 transition-colors"

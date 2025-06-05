2a5dbdcf7ae17291aeba8659d6f7926ca8002031
Keston C. Cole <163892335+KestonCCole5654@users.noreply.github.com>
2025-06-05 00:31:29 -0500

    Updated Supabase Instance

--- client/src/lib/supabase.ts
+++ client/src/lib/supabase.ts
@@ -1,6 +1,13 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient, SupabaseClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL!
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY!
- 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey) 
+
+let supabaseInstance: SupabaseClient | null = null
+
+export const supabase = (() => {
+  if (!supabaseInstance) {
+    supabaseInstance = createClient(supabaseUrl, supabaseAnonKey)
+  }
+  return supabaseInstance
+})() 

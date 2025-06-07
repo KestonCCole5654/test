79fd6fed04f1462920131d8af5a57dba3314b6b8
Keston C. Cole <163892335+KestonCCole5654@users.noreply.github.com>
2025-06-07 10:24:42 -0500

    Update

--- client/src/pages/EmailInvoice/email-invoice.tsx
+++ client/src/pages/EmailInvoice/email-invoice.tsx
@@ -72,7 +72,7 @@ export default function EmailInvoice() {
   return (
     <div className="w-full  max-w-7xl bg-gray-50/50 font-onest  flex flex-col md:flex-row gap-8">
       {/* Left: Email Form */}
-      <div className="flex-1 w-full min-w-0">
+      <div className="flex-1 min-w-0">
         <div className="grid gap-4">
           <div className="grid gap-2">
             <Label htmlFor="from">From</Label>
@@ -154,9 +154,8 @@ export default function EmailInvoice() {
             <div className="text-3xl font-medium text-gray-800 mb-2">
               ${amount?.toFixed(2)}
             </div>
-            <div className="text-xs text-gray-500 mb-1">
-              DUE {dueDate || "—"}
-            </div>
+
+           
             <Button
               variant="default"
               className="bg-gray-800 text-white px-6 py-2 mb-1"
@@ -165,6 +164,10 @@ export default function EmailInvoice() {
             >
               Print or save
             </Button>
+
+            <div className="text-xs text-gray-500 mb-1">
+              DUE {dueDate || "—"}
+            </div>
            
           </div>
           <div className="text-sm whitespace-pre-line mb-2">
